161f80cc821f3a9819621e301f931a843ebb9b01
Raunak Bhanarkar <raunakbhanarkar7@gmail.com>
2023-03-13 07:31:28 +0530

    first commit

--- api/server.js
+++ api/server.js
@@ -11,9 +11,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', async (req,res) => {
-	res.send('Hello from');
-})
 
 mongoose.connect("mongodb+srv://Raunak:Raunak2003@cluster0.spm5oss.mongodb.net/?retryWrites=true&w=majority", {
 	useNewUrlParser: true, 
